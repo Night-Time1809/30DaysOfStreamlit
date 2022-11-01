5aa421a29512e37125b7e30120b20bf2d09a247b
Night-Time1809 <chanoknun.wora@gmail.com>
2022-11-01 09:12:58 +0000

    day12

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,16 @@
 import streamlit as st
 
-st.header("st.multiselect")
+st.header("st.checkbox")
 
-options = st.multiselect(
-     "What are your favorite colors",
-     ["Green", "Yellow", "Red", "Blue"],
-     ["Yellow", "Red"]
-)
+st.write("What would you like to order?")
 
-st.write("Your selected:", options)
+icecream = st.checkbox("Ice cream")
+coffee = st.checkbox("Coffee")
+cola = st.checkbox("Cola")
+
+if icecream:
+    st.write("Great! Here's some more 🍦")
+if coffee:
+    st.write("Okay, here's some coffee ☕")
+if cola:
+    st.write("Here you go 🥤")
